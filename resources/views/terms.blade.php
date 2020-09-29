acddae8016d033bb2943989f2c897fb54ac22ce3
actvmedia <72033303+actvmedia@users.noreply.github.com>
2020-09-29 17:49:10 -0400

    Add files via upload

---
+++ resources/views/terms.blade.php
@@ -0,0 +1,146 @@
+<!-- Stored in resources/views/child.blade.php -->
+
+@extends('layouts.app')
+
+@section('title', 'Results')
+
+
+@section('content')
+<div class="about-hero">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 text-center">
+            <h1>Terms & Conditions</span></h1>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="about-section">
+    <div class="about-section-2">
+      <div class="container">
+          <div class="row">
+              <div class="col-md-4">
+                <div class="review-content">
+                  <h2></h2>
+                  <p></p>
+                  <p>General
+ 
+This website ecomscholar.com (the “Site”) is owned and operated by EcomScholar, LLC (“Company “We” or “Us”). By using the Site, you agree to be bound by these Terms of Service and to use the Site in accordance with these Terms of Service, our Privacy Policy and any additional terms and conditions that may apply to specific sections of the Site or to products and services available through the Site. Accessing the Site, in any manner, whether automated or otherwise, constitutes use of the Site and your agreement to be bound by these Terms of Service.
+ 
+We reserve the right to change these Terms of Service or to impose new conditions on use of the Site, from time to time, in which case we will post the revised Terms of Service on this website. By continuing to use the Site after we post any such changes, you accept the Terms of Service, as modified. 
+ 
+Intellectual Property Rights
+ 
+The Millionaire Mentorship Program and all related materials contained in the Site are owned by the Company, copyrighted and all rights are reserved. Text, graphics, databases, HTML code, and other intellectual property are protected by US and International Copyright Laws, and may not be copied, reprinted, published, reengineered, translated, hosted, or otherwise distributed by any means without explicit permission from the Company. All of the trademarks on this site are trademarks of the Company or of other owners used with their permission.
+ 
+Our Limited License to You
+ 
+The Site is provided solely for your personal noncommercial use. You may not use the Site or the materials available on the Site in a manner that constitutes an infringement of our rights or that has not been authorized by us. More specifically, unless explicitly authorized in these Terms of Service or by the owner of the materials, you may not modify, copy, reproduce, republish, upload, post, transmit, translate, sell, create derivative works, exploit, or distribute in any manner or medium (including by email or other electronic means) any material from the Site. You may, however, from time to time, download and/or print one copy of individual pages of the Site for your personal, non-commercial use, provided that you keep intact all copyright and other proprietary notices.
+ 
+Limitations on Linking and Framing. 
+ 
+You may establish a hypertext link to the Site so long as the link does not state or imply any sponsorship of your site by us or by the Site. However, you may not, without our prior written permission, frame or inline link any of the content of the Site, or incorporate into another website or other service any of our material, content or intellectual property.
+ 
+Disclaimers
+ 
+THE INFORMATION, PRODUCTS AND SERVICES OFFERED ON OR THROUGH THE SITE AND BY THE COMPANY AND ANY THIRD-PARTY SITES ARE PROVIDED “AS IS” AND WITHOUT WARRANTIES OF ANY KIND EITHER EXPRESS OR IMPLIED. TO THE FULLEST EXTENT PERMISSIBLE PURSUANT TO APPLICABLE LAW, WE DISCLAIM ALL WARRANTIES, EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. WE DO NOT WARRANT THAT THE SITE OR ANY OF ITS FUNCTIONS WILL BE UNINTERRUPTED OR ERROR-FREE, THAT DEFECTS WILL BE CORRECTED, OR THAT ANY PART OF THIS SITE, INCLUDING BULLETIN BOARDS, OR THE SERVERS THAT MAKE IT AVAILABLE, ARE FREE OF VIRUSES OR OTHER HARMFUL COMPONENTS.
+ 
+We do not warrant or make any representations regarding the use or the results of the use of the site or materials on this site in terms of their correctness, accuracy, timeliness, reliability or otherwise.
+ 
+Throughout the Site, we may provide links and pointers to Internet sites maintained by third parties. Our linking to such third-party sites does not imply an endorsement or sponsorship of such sites, or the information, products or services offered on or through the sites. In addition, neither we nor affiliates operate or control in any respect any information, products or services that third parties may provide on or through the Site or on websites linked to by us on the Site.
+If applicable, any opinions, advice, statements, services, offers, or other information or content expressed or made available by third parties, including information providers, are those of the respective authors or distributors, and not the Company. Neither the Company nor any third-party provider of information guarantees the accuracy, completeness, or usefulness of any content. Furthermore, the Company neither endorses nor is responsible for the accuracy and reliability of any opinion, advice, or statement made on any of the Sites by anyone other than an authorized the Company representative while acting in his/her official capacity.
+ 
+You agree at all times to defend, indemnify and hold harmless the Company its affiliates, their successors, transferees, assignees and licensees and their respective parent and subsidiary companies, agents, associates, officers, directors, shareholders and employees of each from and against any and all claims, causes of action, damages, liabilities, costs and expenses, including legal fees and expenses, arising out of or related to your breach of any obligation, warranty, representation or covenant set forth herein.
+ 
+Online Commerce
+ 
+Your purchase is for personal use only. Sharing of purchases is not permitted and will be considered unauthorized, an infringing use of our copyrighted material, and may subject violators to liability. 
+ 
+If payment is declined, our system will automatically disable access to the purchased product. (We understand. This usually happens because a credit card expires.) We want to help restore your access, so we’ll make every attempt to contact you to help resolve this issue. Once the billing issue is resolved, we’ll restore access.
+ 
+Interactive Features
+ 
+This Site may include a variety of features, such as bulletin boards, web logs, chat rooms, and email services, which allow feedback to us and real-time interaction between users, and other features which allow users to communicate with others. Responsibility for what is posted on bulletin boards, web logs, chat rooms, and other public posting areas on the Site, or sent via any email services on the Site, lies with each user – you alone are responsible for the material you post or send. We do not control the messages, information or files that you or others may provide through the Site. 
+ 
+It is a condition of your use of the Site that you do not:
+ 
+Restrict or inhibit any other user from using and enjoying the Site.
+Use the Site to impersonate any person or entity, or falsely state or otherwise misrepresent your affiliation with a person or entity.
+Interfere with or disrupt any servers or networks used to provide the Site or its features, or disobey any requirements, procedures, policies or regulations of the networks we use to provide the Site.
+Use the Site to instigate or encourage others to commit illegal activities or cause injury or property damage to any person.
+Gain unauthorized access to the Site, or any account, computer system, or network connected to this Site, by means such as hacking, password mining or other illicit means.
+Obtain or attempt to obtain any materials or information through any means not intentionally made available through this Site.
+Use the Site to post or transmit any unlawful, threatening, abusive, libelous, defamatory, obscene, vulgar, pornographic, profane or indecent information of any kind, including without limitation any transmissions constituting or encouraging conduct that would constitute a criminal offense, give rise to civil liability or otherwise violate any local, state, national or international law.
+Use the Site to post or transmit any information, software or other material that violates or infringes upon the rights of others, including material that is an invasion of privacy or publicity rights or that is protected by copyright, trademark or other proprietary right, or derivative works with respect thereto, without first obtaining permission from the owner or rights holder.
+Use the Site to post or transmit any information, software or other material that contains a virus or other harmful component.
+Use the Site to post, transmit or in any way exploit any information, software or other material for commercial purposes, or that contains advertising.
+Use the Site to advertise or solicit to anyone to buy or sell products or services, or to make donations of any kind, without our express written approval.
+Gather for marketing purposes any email addresses or other personal information that has been posted by other users of the Site.
+ 
+The Company may host message boards, chats and other public forums on its Sites. Any user failing to comply with the terms and conditions of this Agreement may be expelled from and refused continued access to, the message boards, chats or other public forums in the future. The Company or its designated agents may remove or alter any user-created content at any time for any reason. Message boards, chats and other public forums are intended to serve as discussion centers for users and subscribers. Information and content posted within these public forums may be provided by the Company staff, the Company’s outside contributors, or by users not connected with the Company, some of whom may employ anonymous user names. The Company expressly disclaims all responsibility and endorsement and makes no representation as to the validity of any opinion, advice, information or statement made or displayed in these forums by third parties, nor are we responsible for any errors or omissions in such postings, or for hyperlinks embedded in any messages. 
+ 
+Under no circumstances will we, our affiliates, suppliers or agents be liable for any loss or damage caused by your reliance on information obtained through these forums. The opinions expressed in these forums are solely the opinions of the participants, and do not reflect the opinions of the Company or any of its subsidiaries or affiliates.
+ 
+The Company has no obligation whatsoever to monitor any of the content or postings on the message boards, chat rooms or other public forums on the Sites. However, you acknowledge and agree that we have the absolute right to monitor the same at our sole discretion. In addition, we reserve the right to alter, edit, refuse to post or remove any postings or content, in whole or in part, for any reason and to disclose such materials and the circumstances surrounding their transmission to any third party in order to satisfy any applicable law, regulation, legal process or governmental request and to protect ourselves, our clients, sponsors, users and visitors.
+ 
+We occasionally include access to an online community as part of our programs. We want every single member to add value to the group. Our goal is to make your community the most valuable community you’re a member of. Therefore, we reserve the right to remove anyone at any time. We rarely do this, but we want to let you know how seriously we take our communities.
+ 
+Registration
+ 
+To access certain features of the Site, we may ask you to provide certain demographic information including your gender, year of birth, zip code and country. In addition, if you elect to sign-up for a particular feature of the Site, such as chat rooms, web logs, or bulletin boards, you may also be asked to register with us on the form provided and such registration may require you to provide personally identifiable information such as your name and email address. You agree to provide true, accurate, current and complete information about yourself as prompted by the Site’s registration form. If we have reasonable grounds to suspect that such information is untrue, inaccurate, or incomplete, we have the right to suspend or terminate your account and refuse any and all current or future use of the Site (or any portion thereof). Our use of any personally identifiable information you provide to us as part of the registration process is governed by the terms of our Privacy Policy.
+ 
+Passwords
+ 
+To use certain features of the Site, you will need a username and password, which you will receive through the Site’s registration process. You are responsible for maintaining the confidentiality of the password and account, and are responsible for all activities (whether by you or by others) that occur under your password or account. You agree to notify us immediately of any unauthorized use of your password or account or any other breach of security, and to ensure that you exit from your account at the end of each session. We cannot and will not be liable for any loss or damage arising from your failure to protect your password or account information.
+ 
+Limitation of Liability
+ 
+UNDER NO CIRCUMSTANCES, INCLUDING, BUT NOT LIMITED TO, NEGLIGENCE, SHALL COMPANY, ITS SUBSIDIARY AND PARENT COMPANIES OR AFFILIATES BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL OR CONSEQUENTIAL DAMAGES THAT RESULT FROM THE USE OF, OR THE INABILITY TO USE, THE SITE, INCLUDING OUR MESSAGING, BLOGS, COMMENTS OF OTHERS, BOOKS, EMAILS, PRODUCTS, OR SERVICES, OR THIRD-PARTY MATERIALS, PRODUCTS, OR SERVICES MADE AVAILABLE THROUGH THE SITE OR BY US IN ANY WAY, EVEN IF WE ARE ADVISED BEFOREHAND OF THE POSSIBILITY OF SUCH DAMAGES. (BECAUSE SOME STATES DO NOT ALLOW THE EXCLUSION OR LIMITATION OF CERTAIN CATEGORIES OF DAMAGES, THE ABOVE LIMITATION MAY NOT APPLY TO YOU. IN SUCH STATES, OUR LIABILITY AND THE LIABILITY OF OUR SUBSIDIARY AND PARENT COMPANIES OR AFFILIATES IS LIMITED TO THE FULLEST EXTENT PERMITTED BY SUCH STATE LAW.) YOU SPECIFICALLY ACKNOWLEDGE AND AGREE THAT WE ARE NOT LIABLE FOR ANY DEFAMATORY, OFFENSIVE OR ILLEGAL CONDUCT OF ANY USER. IF YOU ARE DISSATISFIED WITH THE SITE, ANY MATERIALS, PRODUCTS, OR SERVICES ON THE SITE, OR WITH ANY OF THE SITE’S TERMS AND CONDITIONS, YOUR SOLE AND EXCLUSIVE REMEDY IS TO DISCONTINUE USING THE SITE AND THE PRODUCTS, SERVICES AND/OR MATERIALS.  
+ 
+In no event shall Company’s total liability to you for all damages, losses, and causes of action (whether in contract, tort, including but not limited to, negligence or otherwise) exceed the amount paid by you to the Company.
+ 
+The company is not an investment advisory service, is not an investment adviser, and does not provide personalized financial advice or act as a financial advisor.
+ 
+We exist for educational purposes only, and the materials and information contained herein and in our products and services are for general informational purposes only. None of the information provided by us is intended as investment, tax, accounting or legal advice, as an offer or solicitation of an offer to buy or sell, or as an endorsement, recommendation or sponsorship of any the company, security, or fund. Our information should not be relied upon for purposes of transacting in securities or other investments.
+ 
+We do not offer or provide tax, legal or investment advice and you are responsible for consulting tax, legal, or financial professionals before acting on any information provided by us. This site is continually under development and the company makes no warranty of any kind, implied or express, as to its accuracy, completeness or appropriateness for any purpose. 
+ 
+YOU acknowledge and agrees that no representation has been made by the Company OR ITS AFFILIATES as to the future income, expenses, sales volume or potential profitability that may be derived from your participation in the EcomScholar Mentorship program.
+ 
+Termination
+ 
+We may cancel or terminate your right to use the Site or any part of the Site at any time without notice. In the event of cancellation or termination, you are no longer authorized to access the part of the Site affected by such cancellation or termination. The restrictions imposed on you with respect to material downloaded from the Site, and the disclaimers and limitations of liabilities set forth in these Terms of Service, shall survive.
+ 
+Refund Policy
+ 
+A 30 day money back guarantee applies to your initial purchase on the Site. This will give you 30 days to try the course and cancel at any time for a full refund within 30 days of your first date of purchase.
+ 
+General Terms
+ 
+The Digital Millennium Copyright Act of 1998 (the “DMCA”) provides recourse for copyright owners who believe that material appearing on the Internet infringes their rights under the U.S. copyright law. If you believe in good faith that materials hosted by the Company infringe your copyright, you, or your agent may send to the Company a notice requesting that the material be removed or access to it be blocked. Any notification by a copyright owner or a person authorized to act on its behalf that fails to comply with requirements of the DMCA shall not be considered sufficient notice and shall not be deemed to confer upon the Company actual knowledge of facts or circumstances from which infringing material or acts are evident. If you believe in good faith that a notice of copyright infringement has been wrongly filed against you, the DMCA permits you to send to the Company a counter-notice. All notices and counter notices must meet the then current statutory requirements imposed by the DMCA; see http://www.loc.gov/copyright for details. The Company’s Copyright Agent for notice of claims of copyright infringement or counter notices can be reached as follows: ecomscholar.biz@gmail.com.
+ 
+This Agreement shall be binding upon and inure to the benefit of the Company and our respective assigns, successors, heirs, and legal representatives. Neither this Agreement nor any rights hereunder may be assigned without the prior written consent of the Company. Notwithstanding the foregoing, all rights and obligations under this Agreement may be freely assigned by the Company to any affiliated entity or any of its wholly owned subsidiaries. 
+ 
+These Terms of Service shall be governed by and construed in accordance with the laws of the State of California and any dispute shall be subject to final binding arbitration before a single arbitrator in Orange County, California pursuant to the Rules of the American Arbitration Association. If any provision of this agreement shall be held unlawful, void or for any reason unenforceable, then that provision shall be deemed severable from this agreement and shall not affect the validity and enforceability of any remaining provisions.</p>
+                </div>
+              </div>
+     
+                </div>
+              </div>
+          </div>
+      </div>
+    </div>
+  </div>
+
+        <div class="more-info white-bg">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <h2>Covering the Basics to Advanced</h2>
+                        <p>Whether you're a complete beginner or an experienced seller, EcomScholar has you covered.</p>
+                        <a href="/register" class="btn btn-def">Explore Our Training</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+@endsection
